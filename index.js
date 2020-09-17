a399ef561dcd75d734bd339b1cf841fa789ee572
Mohammed Abbasi <raamizabbasi@Mohammeds-MacBook-Pro.local>
2020-09-17 19:07:45 -0400

    Added functionality for reading words from a file and then getting their definitions

--- index.js
+++ index.js
@@ -8,38 +8,72 @@ const rl = readline.createInterface({
 
 const baseURL = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
 
-const operate = async (array, write) => {
-	array.forEach(async (word) => {
-		const updatedWord = word.charAt(0).toUpperCase() + word.slice(1)
-		try {
-			const result = await axios.get(baseURL + word)
-			const definition = result.data[0].meanings[0].definitions[0].definition
-			const string = `\n${updatedWord}: ${definition}\n`
-			if (write === 'y') {
-				fs.appendFile('definitions.txt', string, () => {
-					console.log(string)
+const operate = async (words, filePath) => {
+	rl.question(
+		'\nDo you want to write the definitions into a file (y/n)? ',
+		(writeFile) => {
+			if (writeFile === 'y') {
+				fs.truncate(filePath || 'definitions.txt', 0, (err) => {
+					if (err) {
+						console.log('Unable to delete data in the file')
+					}
 				})
 			}
-		} catch (err) {
-			const errorString = `\nError in retrieving the definition of the word: ${updatedWord}\n`
-			if (write === 'y') {
-				fs.appendFile('definitions.txt', errorString, () => {
-					console.log(errorString)
-				})
-			}
+			words.forEach(async (word) => {
+				const updatedWord = word.charAt(0).toUpperCase() + word.slice(1)
+				let string
+				try {
+					const result = await axios.get(baseURL + word)
+					const definition = result.data[0].meanings[0].definitions[0].definition
+					const writeString = `\n${updatedWord}: ${definition}\n`
+					string = writeString
+				} catch (err) {
+					const errorString = `\nError in finding the definition for the word: ${word}\n`
+					string = errorString
+				}
+				if (writeFile === 'y') {
+					fs.appendFile(filePath || 'definitions.txt', string, (err) => {
+						if (err) {
+							console.error('Error writing to file for the word ' + word)
+						}
+					})
+				}
+				console.log(string)
+			})
 		}
-	})
+	)
 }
 
 rl.question(
-	'Input all the words you want the definition of with a comma and space separating each word: ',
-	(rawWords) => {
-		const words = rawWords.split(', ')
-		rl.question(
-			'Do you want to write the definitions in a file? (y/n) ',
-			(writeToFile) => {
-				operate(words, writeToFile)
-			}
-		)
+	'\nDo you want to type the words, or do  you want to get the words from a file (type/read)? ',
+	(typeOrRead) => {
+		if (typeOrRead === 'type') {
+			rl.question(
+				'\nType the words you want the definitions of by separating each word with a comma and space: ',
+				(rawWords) => {
+					const words = rawWords.split(', ')
+					operate(words, 'definitions.txt')
+				}
+			)
+		} else if (typeOrRead === 'read') {
+			rl.question(
+				'\nType the exact name of the file in this directory that you want to read from (ex: definitions.txt): ',
+				(filePath) => {
+					fs.readFile(filePath, 'utf8', (err, data) => {
+						if (err) {
+							console.log(
+								'Error reading the file. Please make sure it is in the same directory and that there are no restrictions on it'
+							)
+						} else {
+							const words = data.split('\n')
+							operate(words, filePath)
+						}
+					})
+				}
+			)
+		} else {
+			console.log('You entered an incorrect answer. Please run the program again')
+			process.exit(1)
+		}
 	}
 )
